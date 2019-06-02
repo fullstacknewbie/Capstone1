6e955d244cffa5ec6d7e7ef6f91fcba9d1ccf52f
fullstacknewbie <49616863+fullstacknewbie@users.noreply.github.com>
2019-06-02 11:05:52 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function watchForSubmit() {
     })
 }
 
+//converts zip code into coordinates
 function getLatLong(zip){
     fetch ("https://maps.googleapis.com/maps/api/geocode/json?address="+zip+"&key=AIzaSyAqHEFAOikP1X73U_trS-aQvjiqcXKWSHs")
         .then(response=>response.json())
         .then(responseJson=>getResults(responseJson))
 }
 
+//feeds coordinates into Open Charge API to retrieve nearest charging locations
 function getResults(responseJson) {
     initMap(responseJson)
     let lat=responseJson.results[0].geometry.location.lat
@@ -29,16 +31,21 @@ function getResults(responseJson) {
         .then(responseLatLong=>displayResults(responseLatLong))
 }
 
+//displays map and list of results
 function displayResults(responseLatLong){
     let total=responseLatLong.length
     let i=0
+    //iterates over all results
     for (i=0; i<total; i++) {
+        //displays current address on Google Map
         var marker = new google.maps.Marker({position: latLong, map: map, icon: 'marker.png'});
+        //defines address components
         var address=responseLatLong[i].AddressInfo.AddressLine1
         var city=responseLatLong[i].AddressInfo.Town
         var state=responseLatLong[i].AddressInfo.StateOrProvince
         var zipCode=responseLatLong[i].AddressInfo.Postcode
         var latLong = {lat: responseLatLong[i].AddressInfo.Latitude, lng: responseLatLong[i].AddressInfo.Longitude}
+        //returns list of addresses to webpage
         $("#results").append("<span>"+address+"<br>"+city+", "+zipCode+" "+state+"<span><br><br>");
     }
 }
